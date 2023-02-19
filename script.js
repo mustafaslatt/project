38ce5b15ed5d1bce66e425294cdbdb7f8fb2268e
Mustafa <slimebossnazarbek@gmail.com>
2023-02-19 16:56:56 +0300

    newcom

--- script.js
+++ script.js
@@ -1,4 +1,12 @@
-let numberOfFilms = prompt('Сколько фильмов вы уже посмотрели?', '');
+'use strict'
+let numberOfFilms;
+function start() {
+  numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+  while (numberOfFilms == '' || numberOfFilms == null || isNaN(numberOfFilms)) {
+    numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+  }
+}
+start()
 let personalMovieDB = {
   count: numberOfFilms,
   movies: {},
@@ -6,12 +14,45 @@ let personalMovieDB = {
   genres: [],
   privat: false
 };
-let a = prompt('Один из просмотренных фильмов?', '');
-let b = prompt('На сколько оцените его?', '');
-let c = prompt('Один из просмотренных фильмов?', '');
-let d = prompt('На сколько оцените его?', '');
-
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
-
+function rememberMyFilms() {
+  for (let i = 0; i < 2; i++) {
+    let a = prompt('Один из просмотренных фильмов?', '');
+    let b = prompt('На сколько оцените его?', '');
+    
+    if (a != null && b != null && a != '' && b != '' && a.length < 50 && b.length < 50) {
+      personalMovieDB.movies[a] = b;
+      console.log('done')
+    } else {
+      console.log('error')
+      i--;
+    }
+  }
+}
+rememberMyFilms()
+function detectPersonalLevel() {
+  if (personalMovieDB.count < 10) {
+    console.log('Просмотрено довольно мало фильмов')
+  } else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+    console.log('Вы классический зритель')
+  } else if (personalMovieDB.count >= 30) {
+    console.log('Вы киноман')}
+    else {
+      console.log('Произошла ошибка')
+    }
+      
+}
+detectPersonalLevel()
+function showMyDB(hidden) {
+if (!hidden) {
+  console.log(personalMovieDB)
+}
+}
+showMyDB(personalMovieDB.privat)
+function writeYourGenres() {
+  for (let i = 1; i <= 3; i++) {
+    let genre = prompt(`Ваш любимый жанр под номером ${i}`)
+    personalMovieDB.genres[i - 1] = genre
+  }
+}
+writeYourGenres()
 console.log(personalMovieDB)
