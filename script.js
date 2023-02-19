4e495d73e709679f6f7f2673db271e5aa1b1d38c
Mustafa <slimebossnazarbek@gmail.com>
2023-02-19 04:21:39 +0300

    firstapp

--- script.js
+++ script.js
@@ -1,7 +1,17 @@
-let storeName = 'slatt';
-let storeDescription = {
-  budget: 10000,
-  employees: ['Adam', 'Musa', 'Mustafa'],
-  products: {snickers: 50, twix: 40},
-  open: true
-}
+let numberOfFilms = prompt('Сколько фильмов вы уже посмотрели?', '');
+let personalMovieDB = {
+  count: numberOfFilms,
+  movies: {},
+  actors: {},
+  genres: [],
+  privat: false
+};
+let a = prompt('Один из просмотренных фильмов?', '');
+let b = prompt('На сколько оцените его?', '');
+let c = prompt('Один из просмотренных фильмов?', '');
+let d = prompt('На сколько оцените его?', '');
+
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
+
+console.log(personalMovieDB)
